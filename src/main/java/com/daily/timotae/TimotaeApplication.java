e68217b95b64a4f19f263da29ed43020ab08d7ed
gnoesnooj <306yyy@naver.com>
2021-12-21 18:48:00 +0900

    chore/setting

---
+++ src/main/java/com/daily/timotae/TimotaeApplication.java
@@ -0,0 +1,13 @@
+package com.daily.timotae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimotaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimotaeApplication.class, args);
+	}
+
+}
