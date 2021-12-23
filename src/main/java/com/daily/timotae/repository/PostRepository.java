f17eafbf2ef0b14a1ea2a9fc59c77b1d686e5860
gnoesnooj <306yyy@naver.com>
2021-12-23 23:56:39 +0900

    Feat : 전체 조회를 위한 findAll() 추가

--- src/main/java/com/daily/timotae/repository/PostRepository.java
+++ src/main/java/com/daily/timotae/repository/PostRepository.java
@@ -2,6 +2,9 @@ package com.daily.timotae.repository;
 
 import com.daily.timotae.domain.Post;
 
+import java.util.List;
+
 public interface PostRepository {
     Post savePost(Post post);
+    List<Post> findAll();
 }
