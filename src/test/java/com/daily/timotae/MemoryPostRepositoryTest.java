e2cfddba4851bbd07aea6bc4e5347e903cb30326
gnoesnooj <306yyy@naver.com>
2021-12-24 00:22:45 +0900

    Test : MemoryPostRepository의 savePost(글 저장) 기능에 대한 Test

---
+++ src/test/java/com/daily/timotae/MemoryPostRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.daily.timotae;
+
+import com.daily.timotae.domain.Post;
+import com.daily.timotae.repository.MemoryPostRepository;
+import com.daily.timotae.repository.PostRepository;
+import com.daily.timotae.service.BoardService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class MemoryPostRepositoryTest {
+
+    PostRepository postRepository = new MemoryPostRepository();
+
+    @AfterEach
+    public void afterEach(){
+        postRepository.clearStore();
+    }
+
+    @Test
+    public void 글작성(){
+        //given
+        new Post();
+
+        String title = "제목";
+        String content = "글 내용";
+        String userId = "작성자";
+        String category = "카테고리";
+        String dateOfIssue = "2021-12-23";
+
+        Post post = Post.builder()
+                .title(title)
+                .category(category)
+                .content(content)
+                .userId(userId)
+                .dateOfIssue(dateOfIssue)
+                .build();
+
+        postRepository.savePost(post);
+
+        //when
+        List<Post> result = postRepository.findAll();
+
+        //then
+        Assertions.assertThat(result.size()).isEqualTo(1);
+    }
+}
