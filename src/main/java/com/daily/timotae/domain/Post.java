b10843c9b3c749916461b0e66948dc19a3d93ab4
gnoesnooj <306yyy@naver.com>
2021-12-26 20:11:43 +0900

    refactor : @Builder 사용하지 않고 빌더 패턴 적용하기

--- src/main/java/com/daily/timotae/domain/Post.java
+++ src/main/java/com/daily/timotae/domain/Post.java
@@ -5,48 +5,63 @@ import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
 
-public class Post implements Serializable {
+public class Post {
 
-    // @Id
+    @Id
     private Long postId;
+
     private String title;
+
     private String category;
+
     private String userId;
+
     private String dateOfIssue;
+
     private String content;
 
-    public Post(){ }
-
-    public Long getPostId() {
-        return postId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
+    public Long getPostId() { return postId; }
+    public String getTitle() { return title; }
     public String getCategory() {
         return category;
     }
-
     public String getUserId() {
         return userId;
     }
-
     public String getDateOfIssue() {
         return dateOfIssue;
     }
-
     public String getContent() {
         return content;
     }
 
-    @Builder
-    public Post(String title, String category, String userId, String dateOfIssue, String content){
+    private Post(PostBuilder postBuilder) {
+        this.postId = postId;
         this.title = title;
         this.category = category;
         this.userId = userId;
         this.dateOfIssue = dateOfIssue;
         this.content = content;
+    }
+
+    public static class PostBuilder{
+
+        private String title;
+        private String category;
+        private String userId;
+        private String dateOfIssue;
+        private String content;
+
+        public PostBuilder(String title, String category, String userId, String dateOfIssue, String content) {
+            this.title = title;
+            this.category = category;
+            this.userId = userId;
+            this.dateOfIssue = dateOfIssue;
+            this.content = content;
+        }
+
+        public Post build() {
+            return new Post(this);
+        }
     }
 }
