fdb7f3c96a51e9fd1f43337953bcd3108a0a08d8
we4sley <galaxy138742@gmail.com>
2022-01-11 18:22:34 +0900

    feat: UserController 생성

---
+++ src/main/java/com/daily/timotae/user/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.daily.timotae.user.controller;
+
+import com.daily.timotae.user.domain.User;
+import com.daily.timotae.user.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public List<User> getAllUsers() {
+        return userService.viewUserAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findUserById(@PathVariable Long id) {
+        return userService.findUserById(id);
+    }
+
+    @PostMapping("")
+    public User joinUser(@RequestBody User user) {
+        return userService.joinUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public void updateUser(@PathVariable Long id, @RequestBody User user) {
+        userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeUser(@PathVariable Long id) {
+        userService.removeUser(id);
+    }
+}
