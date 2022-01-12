e1cb5b09097e36c198d8011f2dde7d8b6cf87ac7
gnoesnooj <306yyy@naver.com>
2022-01-13 03:04:06 +0900

    refactor : Annotation 활용 코드 리팩토링

--- src/main/java/com/daily/timotae/domain/Post.java
+++ src/main/java/com/daily/timotae/domain/Post.java
@@ -1,14 +1,23 @@
 package com.daily.timotae.domain;
 
 import lombok.Builder;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.io.Serializable;
 
+@Getter
+@NoArgsConstructor
+@Entity
 public class Post {
 
-//    @Id
-//    private Long postId;
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long postId;
 
     private String title;
 
@@ -20,46 +29,12 @@ public class Post {
 
     private String content;
 
-    public String getTitle() { return title; }
-    public String getCategory() {
-        return category;
-    }
-    public String getUserId() {
-        return userId;
-    }
-    public String getDateOfIssue() {
-        return dateOfIssue;
-    }
-    public String getContent() {
-        return content;
-    }
-
-    private Post(PostBuilder postBuilder) {
-        this.title = postBuilder.title;
-        this.category = postBuilder.category;
-        this.userId = postBuilder.userId;
-        this.dateOfIssue = postBuilder.dateOfIssue;
-        this.content = postBuilder.content;
-    }
-
-    public static class PostBuilder{
-
-        private String title;
-        private String category;
-        private String userId;
-        private String dateOfIssue;
-        private String content;
-
-        public PostBuilder(String title, String category, String userId, String dateOfIssue, String content) {
-            this.title = title;
-            this.category = category;
-            this.userId = userId;
-            this.dateOfIssue = dateOfIssue;
-            this.content = content;
-        }
-
-        public Post build() {
-            return new Post(this);
-        }
+    @Builder
+    public Post(String title, String category, String userId, String dateOfIssue, String content) {
+        this.title = title;
+        this.category = category;
+        this.userId = userId;
+        this.dateOfIssue = dateOfIssue;
+        this.content = content;
     }
 }
