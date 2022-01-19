d2ca71d0bcabb920d86f0fefa2bdd679d2769e85
gnoesnooj <306yyy@naver.com>
2022-01-19 14:18:10 +0900

    refactor : 리턴타입 변경, 테스트 코드 위한 메소드 삭제

--- src/main/java/com/daily/timotae/repository/PostRepository.java
+++ src/main/java/com/daily/timotae/repository/PostRepository.java
@@ -3,17 +3,14 @@ package com.daily.timotae.repository;
 import com.daily.timotae.domain.Post;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 public interface PostRepository {
 
     Post savePost(Post post);
-    Map<Long, Post> findPostAll();
-    Post findPostOne(Long postId);
+    List<Post> findPostAll();
+    Optional<Post> findPostOne(Long postId);
     void changePost(Long postId, Post post);
     void removePost(Long postId);
-
-    List<Post> findAll();
-    void clearStore();
 
 }
