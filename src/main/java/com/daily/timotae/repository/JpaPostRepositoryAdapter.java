c7dc9f295b697c4e344ce2156fd16fc7fef2159b
gnoesnooj <306yyy@naver.com>
2022-01-21 18:51:45 +0900

    feat : 어댑터 패턴, JpaRepository 활용한 리포지토리 구현

---
+++ src/main/java/com/daily/timotae/repository/JpaPostRepositoryAdapter.java
@@ -0,0 +1,41 @@
+package com.daily.timotae.repository;
+
+import com.daily.timotae.domain.Post;
+import org.springframework.context.annotation.Primary;
+
+import java.util.List;
+import java.util.Optional;
+
+@Primary
+public class JpaPostRepositoryAdapter implements PostRepository {
+
+    private JpaPostRepository jpaPostRepository;
+
+    @Override
+    public Post savePost(Post post) {
+        return jpaPostRepository.save(post);
+    }
+
+    @Override
+    public List<Post> findPostAll() {
+        return jpaPostRepository.findAll();
+    }
+
+    @Override
+    public Optional<Post> findPostOne(Long postId) {
+        return jpaPostRepository.findById(postId);
+    }
+
+    @Override
+    public void changePost(Long postId, Post post) {
+        Post tmpPost = findPostOne(postId)
+                .orElseThrow( () -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다. postId : " + postId));
+        tmpPost.update(post.getTitle(), post.getCategory(), post.getUserId(), post.getDateOfIssue(), post.getContent());
+    }
+
+    @Override
+    public void removePost(Long postId) {
+        jpaPostRepository.deleteById(postId);
+    }
+
+}
