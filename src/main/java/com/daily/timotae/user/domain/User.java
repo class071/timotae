bee161d037510842fd17a68be51346a4d82159be
we4sley <galaxy138742@gmail.com>
2022-01-27 02:19:46 +0900

    feat: 필드 조건 설정, 패스워드 encode 메소드 추가, 권한 구분을 위한 Role 필드 추가

--- src/main/java/com/daily/timotae/user/domain/User.java
+++ src/main/java/com/daily/timotae/user/domain/User.java
@@ -1,28 +1,42 @@
 package com.daily.timotae.user.domain;
 
-import lombok.Getter;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 @Entity
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(nullable = false, unique = true)
     private String userId;
 
-    @Column
     private String userPassword;
 
-    @Column
+    @Column(nullable = false)
     private String name;
 
-    @Column
+    @Column(nullable = false)
     private int age;
 
-    @Column
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public void updatePassword(PasswordEncoder passwordEncoder, String userPassword) {
+        this.userPassword = passwordEncoder.encode(userPassword);
+    }
+
+    public void encodePassword(PasswordEncoder passwordEncoder) {
+        this.userPassword = passwordEncoder.encode(userPassword);
+    }
 }
