b42ebe19cc32ebd579b80dfb5b089a3e9334cedf
we4sley <galaxy138742@gmail.com>
2022-02-07 20:37:46 +0900

    feat: jpaRepository 어댑터 패턴 적용

---
+++ src/main/java/com/daily/timotae/user/repository/JpaUserRepositoryAdapter.java
@@ -0,0 +1,40 @@
+package com.daily.timotae.user.repository;
+
+import com.daily.timotae.user.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.daily.timotae.constant.UserConstant.USER_NOT_EXIST;
+
+public class JpaUserRepositoryAdapter implements UserRepository {
+
+    private JpaUserRepository jpaUserRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return jpaUserRepository.save(user);
+    }
+
+    @Override
+    public List<User> findUserAll() {
+        return jpaUserRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findUserOne(Long id) {
+        return jpaUserRepository.findById(id);
+    }
+
+    @Override
+    public void updateUser(Long id, User user) {
+        User newUser = findUserOne(id)
+                .orElseThrow( () -> new IllegalArgumentException(USER_NOT_EXIST + id));
+        newUser.update(user.getUserId(), user.getUserPassword(), user.getName(), user.getAge(), user.getEmail(), user.getRole());
+    }
+
+    @Override
+    public void removeUser(Long id) {
+        jpaUserRepository.deleteById(id);
+    }
+}
