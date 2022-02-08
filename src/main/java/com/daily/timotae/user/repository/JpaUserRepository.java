3117702d2546740f29b262987195d02519554721
we4sley <galaxy138742@gmail.com>
2022-02-08 19:45:39 +0900

    feat: userId를 통한 조회 메소드 추가

--- src/main/java/com/daily/timotae/user/repository/JpaUserRepository.java
+++ src/main/java/com/daily/timotae/user/repository/JpaUserRepository.java
@@ -3,5 +3,9 @@ package com.daily.timotae.user.repository;
 import com.daily.timotae.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaUserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserId(String userId);
 }
