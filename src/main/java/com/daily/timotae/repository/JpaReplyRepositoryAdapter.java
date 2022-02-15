52296f45a903f4f5ce9ad51b958cddc0d2b2eb74
gnoesnooj <306yyy@naver.com>
2022-02-16 02:52:11 +0900

    feat : 댓글 리포지토리 구현 및 어댑터 패턴 적용

---
+++ src/main/java/com/daily/timotae/repository/JpaReplyRepositoryAdapter.java
@@ -0,0 +1,47 @@
+package com.daily.timotae.repository;
+
+import com.daily.timotae.domain.Post;
+import com.daily.timotae.domain.Reply;
+
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+
+import static com.daily.timotae.constant.PostConstant.POST_NOT_EXIST;
+import static com.daily.timotae.constant.ReplyConstant.REPLY_NOT_EXIST;
+
+public class JpaReplyRepositoryAdapter implements ReplyRepository{
+
+    private JpaReplyRepository jpaReplyRepository;
+
+    @Override
+    public Reply saveReply(Reply reply) {
+        return jpaReplyRepository.save(reply);
+    }
+
+    @Override
+    public void deleteReply(Long replyId) {
+        jpaReplyRepository.deleteById(replyId);
+    }
+
+    @Override
+    public void updateReply(Long replyId, Reply reply) {
+        Reply newReply = findById(replyId)
+                .orElseThrow( () -> new IllegalArgumentException(REPLY_NOT_EXIST + replyId));
+        newReply.update(reply.getReplyContent());
+    }
+
+    private Optional<Reply> findById(Long replyId) {
+        return jpaReplyRepository.findById(replyId);
+    }
+
+    @Override
+    public List<Reply> findAllByPostId(long postId, Pageable pageable) {
+        return jpaReplyRepository.findAllByPostId(postId, pageable);
+    }
+
+    @Override
+    public List<Reply> findAllByParentReplyId(long parentReplyId, Pageable pageable) {
+        return jpaReplyRepository.findAllByParentReplyId(parentReplyId, pageable);
+    }
+}
---
+++ src/main/java/com/daily/timotae/repository/ReplyRepository.java
@@ -0,0 +1,15 @@
+package com.daily.timotae.repository;
+
+import com.daily.timotae.domain.Reply;
+
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReplyRepository {
+    Reply saveReply(Reply reply);
+    void deleteReply(Long replyId);
+    void updateReply(Long replyId, Reply reply);
+    List<Reply> findAllByPostId(long postId, Pageable pageable);
+    List<Reply> findAllByParentReplyId(long parentReplyId, Pageable pageable);
+}
