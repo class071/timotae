2537b007687c813f90415bc2ce78f6e80d9146e6
we4sley <galaxy138742@gmail.com>
2022-03-02 16:56:13 +0900

    feat: 로그인 성공 시 토큰 발급 기능 추가

--- src/main/java/com/daily/timotae/global/login/handler/LoginSuccessJWTProvideHandler.java
+++ src/main/java/com/daily/timotae/global/login/handler/LoginSuccessJWTProvideHandler.java
@@ -1,5 +1,8 @@
 package com.daily.timotae.global.login.handler;
 
+import com.daily.timotae.global.jwt.service.JwtService;
+import com.daily.timotae.user.repository.JpaUserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,14 +13,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @Slf4j
 public class LoginSuccessJWTProvideHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private final JwtService jwtService;
+    private final JpaUserRepository jpaUserRepository;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        log.info("로그인에 성공합니다 JWT를 발급합니다. userId: {}", userDetails.getUsername());
+        String username = extractUsername(authentication);
+        String accessToken = jwtService.createAccessToken(username);
+        String refreshToken = jwtService.createRefreshToken();
 
-        response.getWriter().write("Success");
+        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+
+        jpaUserRepository.findByUserId(username).ifPresent(
+                user -> user.updateRefreshToken(refreshToken)
+        );
+
+        log.info( "로그인에 성공합니다. username: {}" ,username);
+        log.info( "AccessToken 을 발급합니다. AccessToken: {}" ,accessToken);
+        log.info( "RefreshToken 을 발급합니다. RefreshToken: {}" ,refreshToken);
+    }
+
+    private String extractUsername(Authentication authentication){
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
     }
 }
