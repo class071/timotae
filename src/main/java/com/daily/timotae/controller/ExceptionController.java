6d8a3689f7006d276700dee5dd3faf29da9e27c6
gnoesnooj <306yyy@naver.com>
2022-03-06 18:47:57 +0900

    refactor : enum 에러코드 활용한 리팩토링

--- src/main/java/com/daily/timotae/controller/ExceptionController.java
+++ src/main/java/com/daily/timotae/controller/ExceptionController.java
@@ -1,10 +1,10 @@
 package com.daily.timotae.controller;
 
+import com.daily.timotae.constant.ErrorCode;
 import com.daily.timotae.dto.ErrorResponse;
 import com.daily.timotae.exception.post.NotSupportSuchTypeException;
 import com.daily.timotae.exception.reply.NoMoreReply;
 import com.daily.timotae.exception.reply.NoParentReplyExist;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,39 +14,40 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class ExceptionController extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e){
+        final ErrorCode errorCode = ErrorCode.RESOURCE_NOT_FOUND;
+        return handleExceptionInternal(errorCode, e.getMessage());
+    }
+
     @ExceptionHandler(NotSupportSuchTypeException.class)
-    public ResponseEntity<?> handleNotSupportSuchTypeException(NotSupportSuchTypeException e){
-        final ErrorResponse errorResponse = ErrorResponse.builder()
-                .code("Not Support Such Type")
-                .message(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    public ResponseEntity<Object> handleNotSupportSuchTypeException(NotSupportSuchTypeException e){
+        final ErrorCode errorCode = ErrorCode.RESOURCE_NOT_FOUND;
+        return handleExceptionInternal(errorCode, e.getMessage());
     }
 
     @ExceptionHandler(NoParentReplyExist.class)
-    public ResponseEntity<?> handleNoParentReplyExist(NoParentReplyExist e){
-        final ErrorResponse errorResponse = ErrorResponse.builder()
-                .code("NOT_EXIST")
-                .message(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    public ResponseEntity<Object> handleNoParentReplyExist(NoParentReplyExist e){
+        final ErrorCode errorCode = ErrorCode.RESOURCE_NOT_FOUND;
+        return handleExceptionInternal(errorCode, e.getMessage());
     }
 
     @ExceptionHandler(NoMoreReply.class)
-    public ResponseEntity<?> handleNoMoreReply(NoMoreReply e){
-        final ErrorResponse errorResponse = ErrorResponse.builder()
-                .code("Not able to create More Reply")
-                .message(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    public ResponseEntity<Object> handleNoMoreReply(NoMoreReply e){
+        final ErrorCode errorCode = ErrorCode.RESOURCE_NOT_FOUND;
+        return handleExceptionInternal(errorCode, e.getMessage());
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e){
-        final ErrorResponse errorResponse = ErrorResponse.builder()
-                .code("Resource not exists")
-                .message("Resource not exists")
-                .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(makeErrorResponse(errorCode, message));
     }
+
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode, String message) {
+        return ErrorResponse.builder()
+                .code(errorCode.name())
+                .message(message)
+                .build();
+    }
+
 }
