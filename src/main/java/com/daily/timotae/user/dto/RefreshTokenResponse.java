74609191a4de2cf79cde440b412a46b487e5f823
we4sley <galaxy138742@gmail.com>
2022-04-08 15:35:07 +0900

    feat: accessToken 재발급 dto

---
+++ src/main/java/com/daily/timotae/user/dto/RefreshTokenResponse.java
@@ -0,0 +1,10 @@
+package com.daily.timotae.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RefreshTokenResponse {
+    private String accessToken;
+}
